c7b59b042b742f0187561eb24bfc82a08c4e2dc6
Jedsada Tiwongvorakul <pondthaitay@gmail.com>
2015-06-28 00:00:02 +0700

    Increment Circlepage

---
+++ app/src/main/java/com/cm_smarthome/materialdesignsupportlibrary/MainCirclepage.java
@@ -0,0 +1,44 @@
+package com.cm_smarthome.materialdesignsupportlibrary;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import me.relex.circleindicator.CircleIndicator;
+
+/**
+ * Created by AdminPond on 27/6/2558.
+ */
+public class MainCirclepage extends FragmentActivity {
+    MyPageAdapter adapter;
+    ViewPager pager;
+    Context context = this;
+    Sqlite sqlite = new Sqlite(context);
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_circlepage);
+
+        sqlite.getWritableDatabase();
+        sqlite.InsertData();
+
+        String[] arrData = sqlite.SelectData("1");
+        if (arrData[0].length() > 0) {
+            String Flag = arrData[1];
+            if (Flag.equals("1")) {
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+
+        adapter = new MyPageAdapter(getSupportFragmentManager());
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
+    }
+}
